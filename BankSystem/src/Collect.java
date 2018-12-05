88c1e43cf18d044e2fced6182b2746fff6ed7a0a
Ajit Vijayakumar <ajitvijayakumar@umail.ucsb.edu>
2018-12-05 06:20:24 -0800

    all transac for atm done

--- BankSystem/src/Collect.java
+++ BankSystem/src/Collect.java
@@ -5,8 +5,39 @@ import java.awt.event.ActionListener;
 import java.sql.*;
 
 public class Collect {
+	public String typeofAccount(String account_id, DatabaseConnection db) {
+		String query = "SELECT account_type FROM Account WHERE account_id = '" + account_id + "'";
+		ResultSet rs = db.querySelect(query);
+		String temp = "";
+		try {
+			while(rs.next()) {
+				String type = rs.getString("account_type");
+				return type;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			
+			e.printStackTrace();
+			return temp;
+		}
+		return temp;
+	}
+	public void collect(String value, String account_id1, String account_id2, DatabaseConnection db) {
+		try {
+			String query2 = "UPDATE Account SET balance = balance - "+value +" WHERE account_id = '"+account_id1.trim()+"'";
+			String query = "UPDATE Account SET balance = balance + "+value +" * .97 WHERE account_id = '"+account_id2.trim()+"'";
+			System.out.println(query2);
+			System.out.println(query);
+			db.queryUpdate(query2);
+			db.queryUpdate(query);
+		}
+		catch (Exception e) {
+	    	  e.printStackTrace();
+	    }
+	}
 	public Collect() {
 		JFrame frame = new JFrame("Collect");
+		DatabaseConnection db = new DatabaseConnection();
 		frame.setSize(800,200);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel panel = new JPanel(); // 
@@ -26,10 +57,20 @@ public class Collect {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				System.out.println(tf.getText() + " sends " + money.getText() + " dollars to " + pin_tf.getText());
-				tf.setText("");
-				money.setText("");
-				pin_tf.setText("");
+				String acc1 = typeofAccount(tf.getText(), db);
+				String acc2 = typeofAccount(pin_tf.getText(), db);
+				System.out.println(acc1);
+				System.out.println(acc2);
+				if(acc1.trim().equals("pocket") && (acc2.trim().equals("interest_checking") || (acc2.trim().equals("student_checking")) || acc2.trim().equals("savings"))) {
+					collect(money.getText(),tf.getText(),pin_tf.getText(),db);
+					frame.dispose();
+					new ATMFunctions();
+					System.out.println(tf.getText() + " pays " + pin_tf.getText() + " " + money.getText() + " dollars");
+				}
+				else {
+					System.out.println("Both accounts must be pocket accounts");
+					frame.dispose();
+				}
 			}
 			
 		});
